99a284a482386253a5c1e131d0c0eb1cdbda193f
Chris Brame <polonel@gmail.com>
2021-10-24 13:53:15 -0400

    Initial

---
+++ copyconfigs.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+CONFDIR=etc/skel
+
+rm -rf etc
+mkdir -p $CONFDIR/.config
+mkdir -p $CONFDIR/.config/conky
+mkdir -p $CONFDIR/.config/fish
+mkdir -p $CONFDIR/.local
+
+cp -R ~/.config/alacritty/ $CONFDIR/.config/
+cp -R ~/.config/conky/doomone-xmonad.conkyrc $CONFDIR/.config/conky/
+cp -R ~/.config/doom/ $CONFDIR/.config/
+cp -R ~/.config/fish/config.fish $CONFDIR/.config/fish/config.fish
+cp -R ~/.config/qutebrowser/ $CONFDIR/.config/
+cp -R ~/.config/sxiv/ $CONFDIR/.config/
+cp -R ~/.config/xmobar/ $CONFDIR/.config/
+
+cp -R ~/.local/bin/ $CONFDIR/.local/
+cp -R ~/.xmonad/ $CONFDIR
+
+cp ~/.bashrc $CONFDIR
+cp ~/.zshrc $CONFDIR
+cp ~/.Xresources $CONFDIR
+cp ~/.wall $CONFDIR
