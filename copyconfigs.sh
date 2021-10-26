cf6b9936b62e0c852674bb7a775fe9b1f4d49845
Chris Brame <polonel@gmail.com>
2021-10-26 17:45:57 -0400

    Updated

--- copyconfigs.sh
+++ copyconfigs.sh
@@ -5,6 +5,8 @@ rm -rf etc
 mkdir -p $CONFDIR/.config
 mkdir -p $CONFDIR/.config/conky
 mkdir -p $CONFDIR/.config/fish
+mkdir -p $CONFDIR/.config/gtk-3.0
+mkdir -p $CONFDIR/.config/volumeicon
 mkdir -p $CONFDIR/.local
 
 cp -R ~/.config/alacritty/ $CONFDIR/.config/
@@ -14,7 +16,8 @@ cp -R ~/.config/fish/config.fish $CONFDIR/.config/fish/config.fish
 cp -R ~/.config/qutebrowser/ $CONFDIR/.config/
 cp -R ~/.config/sxiv/ $CONFDIR/.config/
 cp -R ~/.config/xmobar/ $CONFDIR/.config/
-
+cp ~/.config/volumeicon/volumeicon $CONFDIR/.config/volumeicon
+cp ~/.config/gtk-3.0/settings.ini $CONFDIR/.config/gtk-3.0
 cp -R ~/.local/bin/ $CONFDIR/.local/
 cp -R ~/.xmonad/ $CONFDIR
 
